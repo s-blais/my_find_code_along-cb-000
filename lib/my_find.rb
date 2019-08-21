eaf4f7647fec8212dcd6aa8ccc9b4a3e9287dbcb
Stephen Blais <stephen@s-blais.com>
2019-08-21 17:12:36 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    return collection[i] if yield(collection[i])
+    puts collection[i] if yield(collection[i])
     i = i + 1
   end
 end
